2029113875af21c3de98eb8fbb828557d2db4ad1
tjhv10 <achiyabennatan@gmail.com>
2024-08-25 19:44:53 +0300

    Optimzed the code

--- lib/main.dart
+++ lib/main.dart
@@ -16,39 +16,32 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       home: BlocProvider(
         create: (_) => TextBloc()..add(TextFetched()),
-        child: MyHomePage(),
+        child: const MyHomePage(),
       ),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key});
+  const MyHomePage({super.key});
 
   static const double opacity = 0.58;
-
-  // Define a GlobalKey for the Scaffold
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey, // Assign the GlobalKey to Scaffold
       appBar: AppBar(
         backgroundColor: const Color(0xFFAC9D71).withOpacity(opacity),
-        leading: IconButton(
-          icon: const Icon(Icons.menu, color: Colors.white),
-          onPressed: () {
-            _scaffoldKey.currentState?.openDrawer(); // Open the drawer using GlobalKey
-          },
+        leading: Builder(
+          builder: (context) => IconButton(
+            icon: const Icon(Icons.menu, color: Colors.white),
+            onPressed: () => Scaffold.of(context).openDrawer(),
+          ),
         ),
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const Text(''),
-            _buildCustomSwitch(context),
-          ],
-        ),
+        title: const Spacer(),
+        actions: [
+          _buildCustomSwitch(context),
+        ],
       ),
       drawer: _buildDrawer(context),
       body: _buildBody(context),
@@ -107,27 +100,31 @@ class MyHomePage extends StatelessWidget {
   }
 
   Widget _buildDrawer(BuildContext context) {
-    return BlocBuilder<TextBloc, TextState>(
-      builder: (context, state) {
-        if (state is TextLoadSuccess) {
-          return Drawer(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _buildDrawerButton(context, '1', !state.isText2Selected, () {
-                  context.read<TextBloc>().add(Text1Selected());
-                  Navigator.pop(context);
-                }),
-                _buildDrawerButton(context, '2', state.isText2Selected, () {
-                  context.read<TextBloc>().add(Text2Selected());
-                  Navigator.pop(context);
-                }),
-              ],
-            ),
-          );
-        }
-        return const SizedBox.shrink();
-      },
+    return Drawer(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BlocBuilder<TextBloc, TextState>(
+            builder: (context, state) {
+              if (state is TextLoadSuccess) {
+                return Column(
+                  children: [
+                    _buildDrawerButton(context, '1', !state.isText2Selected, () {
+                      context.read<TextBloc>().add(Text1Selected());
+                      Navigator.pop(context);
+                    }),
+                    _buildDrawerButton(context, '2', state.isText2Selected, () {
+                      context.read<TextBloc>().add(Text2Selected());
+                      Navigator.pop(context);
+                    }),
+                  ],
+                );
+              }
+              return const SizedBox.shrink();
+            },
+          ),
+        ],
+      ),
     );
   }
 
